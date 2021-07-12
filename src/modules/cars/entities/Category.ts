39e390ce22a942fb924197a371dc9accd731c242
Paulo Lima <prmlimajr@hotmail.com>
2021-07-12 01:33:51 -0300

    Postgres integration and migrations

---
+++ src/modules/cars/entities/Category.ts
@@ -0,0 +1,30 @@
+import {
+  Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn,
+} from 'typeorm';
+import { v4 as uuidv4 } from 'uuid';
+
+@Entity('categories')
+class Category {
+  @PrimaryColumn()
+  id?: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidv4();
+    }
+  }
+}
+
+export { Category };
--- src/modules/cars/entities/Specification.ts
+++ src/modules/cars/entities/Specification.ts
@@ -1,10 +1,24 @@
+import {
+  Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn,
+} from 'typeorm';
 import { v4 as uuidv4 } from 'uuid';
 
+@Entity('specifications')
 class Specification {
+  @PrimaryColumn()
   id?: string;
+
+  @Column()
   name: string;
+
+  @Column()
   description: string;
+
+  @CreateDateColumn()
   created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
 
   constructor() {
     if (!this.id) {
